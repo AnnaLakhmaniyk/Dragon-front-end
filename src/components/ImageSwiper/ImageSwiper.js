e4147649c93c2ada47ce4a780c079f186970ee6a
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-09-26 16:17:27 +0300

    component img

--- src/components/ImageSwiper/ImageSwiper.js
+++ src/components/ImageSwiper/ImageSwiper.js
@@ -2,23 +2,28 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper';
-import s from './ImageSwiper.module.css';
+import {
+  Navigation,
+  Pagination,
+  Mousewheel,
+  Keyboard,
+  EffectFade,
+} from 'swiper';
 export const ImageSwiper = ({ images }) => {
   return (
-    <div className={s.wrap}>
+    <div>
       <Swiper
         cssMode={true}
         navigation={true}
         pagination={true}
         mousewheel={true}
         keyboard={true}
-        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard, EffectFade]}
         className="mySwiper"
       >
         {images.map((el, index) => (
           <SwiperSlide key={index}>
-            <img src={el} alt="" width="320px" />
+            <img src={el} alt="" />
           </SwiperSlide>
         ))}
       </Swiper>
