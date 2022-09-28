de9fd2f5cd391c7cc8101294d1d31ee325e1087c
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-09-28 22:10:52 +0300

    add proto

--- src/services/dragonApi.js
+++ src/services/dragonApi.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import PropTypes from 'prop-types';
 
 const getDragon = async () => {
   const data = await axios.get(`/dragons`);
@@ -8,5 +9,8 @@ const getDragon = async () => {
 const getDragonsById = async id => {
   const data = await axios.get(`/dragons/${id}`);
   return data;
+};
+getDragonsById.PropTypes = {
+  id: PropTypes.number,
 };
 export { getDragon, getDragonsById };
