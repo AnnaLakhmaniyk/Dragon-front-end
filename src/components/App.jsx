cad933c1aaa3fae10d4ad584c754248242a7cca8
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-09-28 11:47:47 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Suspense, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { fetchCurrentUser } from '../redax/auth/auth-operations';
 import { getLoder } from '../redax/auth/auth-selector';
 import { ToastContainer } from 'react-toastify';
