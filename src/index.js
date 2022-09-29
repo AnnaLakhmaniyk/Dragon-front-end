8224b1b0270d12d76b7b53249097c147bb1ae3f7
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-09-29 16:15:35 +0300

    daaa

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redax/store';
 import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <GoogleOAuthProvider clientId="446596695369-gcolie52hhf2b943eiba41jefi5raf7q.apps.googleusercontent.com">
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </GoogleOAuthProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
